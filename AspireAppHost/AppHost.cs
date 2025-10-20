c7a2ffb9672c166a080d379e074896ab71afed0f
David Gardiner <david@gardinerfamily.au>
2025-10-20 14:44:24 +1030

    Hosting MongoDB with Aspire

--- AspireAppHost/AppHost.cs
+++ AspireAppHost/AppHost.cs
@@ -1,6 +1,19 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 // MongoDB
+var mongo = builder.AddMongoDB("mongo", 27017)
+    //.WithLifetime(ContainerLifetime.Persistent)
+    .WithDataVolume()
+    .WithMongoExpress();
+
+var mongodb = mongo.AddDatabase("petstore");
+
+var loadData = builder.AddExecutable("load-data", "pwsh", "../mongodb", "-noprofile", "./populate.ps1")
+    .WithReference(mongo)
+    .WaitFor(mongo)
+    .WithArgs("-connectionString")
+    .WithArgs(new ConnectionStringReference(mongo.Resource, false));
+    //.WithExplicitStart();
 
 // Python API
 
