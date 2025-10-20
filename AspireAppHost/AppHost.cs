2fe03aa3ba9d4b439b8d0a63c09248a568aa8339
David Gardiner <david@gardinerfamily.au>
2025-10-20 14:44:24 +1030

    Hosting MongoDB with Aspire
    - Mongo snippets
    - Mongo username/password empty

--- AspireAppHost/AppHost.cs
+++ AspireAppHost/AppHost.cs
@@ -1,6 +1,25 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-// MongoDB
+// begin-snippet: MongoDB
+var mongo = builder.AddMongoDB("mongo", 27017, null, null)
+    .WithEnvironment(context =>
+    {
+        context.EnvironmentVariables.Remove("MONGO_INITDB_ROOT_USERNAME");
+        context.EnvironmentVariables.Remove("MONGO_INITDB_ROOT_PASSWORD");
+    })
+    .WithDataVolume()
+    .WithMongoExpress();
+
+var mongodb = mongo.AddDatabase("petstore");
+// end-snippet: MongoDB
+
+// begin-snippet: PowerShellLoadData
+var loadData = builder.AddExecutable("load-data", "pwsh", "../mongodb", "-noprofile", "./populate.ps1")
+    .WaitFor(mongo)
+    .WithArgs("-connectionString")
+    .WithArgs(new ConnectionStringReference(mongo.Resource, false));
+//.WithExplicitStart();
+// end-snippet: PowerShellLoadData
 
 // Rust service
 
