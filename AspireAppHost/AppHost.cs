871dc58b8b72a9383044057f341dfc6c1eac1a7f
David Gardiner <david@gardinerfamily.au>
2025-10-30 16:03:27 +1030

    Node snippet

--- AspireAppHost/AppHost.cs
+++ AspireAppHost/AppHost.cs
@@ -28,6 +28,7 @@ var rust = builder.AddRustApp("rustpaymentapi", "../RustPaymentApi", [])
 // end-snippet: RustApi
 
 // Node.js App
+// begin-snippet: NodeJokeApi
 var nodeApp = builder.AddPnpmApp("node-joke-api", "../NodeApp")
     // If you are using fnm for Node.js version management, you might need to adjust the PATH
     .WithEnvironment("PATH", Environment.GetEnvironmentVariable("PATH") + ";" + Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\AppData\Roaming\fnm\aliases\default"))
@@ -39,7 +40,8 @@ var launchProfile = builder.Configuration["DOTNET_LAUNCH_PROFILE"];
 if (builder.Environment.IsDevelopment() && launchProfile == "https")
 {
     nodeApp.RunWithHttpsDevCertificate("HTTPS_CERT_FILE", "HTTPS_CERT_KEY_FILE");
-}    
+}
+// end-snippet: NodeJokeApi
 
 // Python API
 // 1. dotnet add package CommunityToolkit.Aspire.Hosting.Python.Extensions
